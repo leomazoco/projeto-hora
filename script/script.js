05cc802df728c632234cf2021a6a71aff5f3512c
Leonardo Mazoco <115808428+leomazoco@users.noreply.github.com>
2022-10-18 14:17:19 -0300

    bug fixed

--- script/script.js
+++ script/script.js
@@ -1,1 +1,22 @@
-window.alert('Olá')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12){
+        //BOM DIA
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18){
+        //BOA TARDE
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#e4b672'
+    } else{
+        //BOA NOITE
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#171927'
+    }
+}
